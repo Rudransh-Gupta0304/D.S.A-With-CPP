81afc7b1f838f8e481a690a252404f889af936df
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2025-12-04 20:56:55 +0530

    initial commit

---
+++ factorial_f.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+       int factorial(int n){
+           int fact=1;
+           for(int i=1;i<=n;i++){
+               fact*=i;
+           }
+         cout<<"The factorial of the given number is "<<fact<<endl;  
+       }
+       
+ int main() {    
+     int num1;
+     cin>> num1;
+     factorial(num1);
+     return 0;
+}
