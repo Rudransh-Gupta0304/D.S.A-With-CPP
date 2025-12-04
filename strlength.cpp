898a1dd9a08afe023342a784b5abc758ce9f6f2e
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2025-12-04 20:46:29 +0530

    initial commit

---
+++ strlength.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a String: ";
+    getline(cin, str);
+    cout << "String length: " << str.length() << endl;
+    return 0;
+}
