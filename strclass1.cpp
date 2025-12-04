0b764684beaec86acb7f64d144cfbc52c31f9fa1
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2025-12-04 20:44:47 +0530

    initial commit

---
+++ strclass1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int main (){
+
+    // string str1, str2;
+    // cout<<" enter 1st string";
+    // getline( cin , str1);
+    // cout<<" enter 2nd string";
+    // getline( cin , str2);
+    // str1 += " " + str2;
+    // cout<< str1 << endl;
+    // return 0;
+
+
+// }
+
+int main(){
+    
+    string str1 = {"hello"};
+    string str2 = {"joy"};
+    string str3 (str2);
+    string str4 (5, 'a');
+    cout<<str1 << endl<< str2 << endl
+        << str3 << endl << str4 << endl;
+    return 0;    
+}
