300edb8f8349cc18b78880195f427a8aac3fed92
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2025-12-04 20:47:21 +0530

    initial commit

---
+++ strconstant.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    string str;
+    cout<<"enter a string: ";
+    getline(cin , str);
+    cout<<"you entered : "<< str <<endl;
+    return 0 ;
+}
